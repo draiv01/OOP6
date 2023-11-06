7714c69d02f8a6e093d772755b8f7aa42997597b
Alexander <119071300+draiv01@users.noreply.github.com>
2023-11-06 15:51:20 +0300

    HW6

---
+++ HW6/OOP6/Teacher.java
@@ -0,0 +1,11 @@
+public abstract class Teacher {
+    //  абстрактный класс 
+    protected String firstName;
+    protected String lastName;
+//  защищённые поля 
+
+    protected Teacher(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
